f914e93c793ab802d56fd34a45b87b194045a63f
remant <remantmahato9798@gmail.com>
2022-02-23 21:14:25 +0530

    [Remant] refactord:implemented the number times head or tail

--- Flipsimulator.sh
+++ Flipsimulator.sh
@@ -1,10 +1,27 @@
 #!/bin/ Bash-x
 
-random=$((RANDOM%2))
-if [ $random -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+
+randomValue=0
+IS_HEAD=0
+IS_TAIL=1
+headNumber=0
+tailNumber=0
+totalFlip=20
+
+while [ $totalFlip -gt 0 ]
+do
+	randomValue=$((RANDOM%2))
+	if [ $randomValue -eq $IS_HEAD ]
+	then
+		((headNumber++))
+		echo "Cong. you got Head"
+	else
+		((tailNumber++))
+		echo "You got tails"
+	fi
+	((totalFlip--))
+done
+
+echo "Number of time head " $headNumber
+echo "Number of time tails " $tailNumber
 
